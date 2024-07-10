8029d5777b58114d5ec48c8c2be65ab5f43f20f6
FrogTheFrog <warliukz@gmail.com>
2024-07-10 22:53:29 +0300

    formatting

--- src/windows/windisplaydevicegeneral.cpp
+++ src/windows/windisplaydevicegeneral.cpp
@@ -6,8 +6,7 @@
 #include "displaydevice/windows/winapiutils.h"
 
 namespace display_device {
-  WinDisplayDevice::
-  WinDisplayDevice(std::shared_ptr<WinApiLayerInterface> w_api):
+  WinDisplayDevice::WinDisplayDevice(std::shared_ptr<WinApiLayerInterface> w_api):
       m_w_api { std::move(w_api) } {
     if (!m_w_api) {
       throw std::logic_error { "Nullptr provided for WinApiLayerInterface in WinDisplayDevice!" };
