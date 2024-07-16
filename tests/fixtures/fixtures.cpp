dd687090125b021063ec3d9fbd71af9b360844a4
FrogTheFrog <warliukz@gmail.com>
2024-07-16 21:43:05 +0300

    feat: Add manual settings test and do some cleanup

--- tests/fixtures/fixtures.cpp
+++ tests/fixtures/fixtures.cpp
@@ -1,13 +1,8 @@
 // header include
 #include "fixtures/fixtures.h"
 
-// local includes
-#include "displaydevice/logging.h"
-
-BaseTest::BaseTest():
-    m_sbuf { nullptr }, m_pipe_stdout { nullptr }, m_pipe_stderr { nullptr } {
-  // intentionally empty
-}
+// system includes
+#include <regex>
 
 void
 BaseTest::SetUp() {
@@ -16,29 +11,14 @@ BaseTest::SetUp() {
     GTEST_SKIP() << skip_reason;
   }
 
-  // todo: only run this one time, instead of every time a test is run
-  // see: https://stackoverflow.com/questions/2435277/googletest-accessing-the-environment-from-a-test
-  // get command line args from the test executable
-  m_test_args = ::testing::internal::GetArgvs();
-
-  // then get the directory of the test executable
-  // std::string path = ::testing::internal::GetArgvs()[0];
-  m_test_binary = m_test_args[0];
-
-  // get the directory of the test executable
-  m_test_binary_dir = std::filesystem::path(m_test_binary).parent_path();
-
-  // If testBinaryDir is empty or `.` then set it to the current directory
-  // maybe some better options here: https://stackoverflow.com/questions/875249/how-to-get-current-directory
-  if (m_test_binary_dir.empty() || m_test_binary_dir.string() == ".") {
-    m_test_binary_dir = std::filesystem::current_path();
+  if (isOutputSuppressed()) {
+    // See https://stackoverflow.com/a/58369622/11214013
+    m_sbuf = std::cout.rdbuf();  // save cout buffer (std::cout)
+    std::cout.rdbuf(m_cout_buffer.rdbuf());  // redirect cout to buffer (std::cout)
   }
 
-  m_sbuf = std::cout.rdbuf();  // save cout buffer (std::cout)
-  std::cout.rdbuf(m_cout_buffer.rdbuf());  // redirect cout to buffer (std::cout)
-
-  // Default to the verbose level in case some test fails
-  display_device::Logger::get().setLogLevel(display_device::Logger::LogLevel::verbose);
+  // Set the default log level, before the test starts. Will default to verbose in case nothing was specified.
+  display_device::Logger::get().setLogLevel(getDefaultLogLevel().value_or(display_device::Logger::LogLevel::verbose));
 }
 
 void
@@ -50,33 +30,51 @@ BaseTest::TearDown() {
     return;
   }
 
-  display_device::Logger::get().setCustomCallback(nullptr);  // restore the default callback to avoid potential leaks
-  std::cout.rdbuf(m_sbuf);  // restore cout buffer
+  // reset the callback to avoid potential leaks
+  display_device::Logger::get().setCustomCallback(nullptr);
 
-  // get test info
-  const ::testing::TestInfo *const test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+  // Restore cout buffer and print the suppressed output out in case we have failed :/
+  if (!isOutputSuppressed()) {
+    std::cout.rdbuf(m_sbuf);
+    m_sbuf = nullptr;
 
-  if (test_info->result()->Failed()) {
-    std::cout << std::endl
-              << "Test failed: " << test_info->name() << std::endl
-              << std::endl
-              << "Captured cout:" << std::endl
-              << m_cout_buffer.str() << std::endl
-              << "Captured stdout:" << std::endl
-              << m_stdout_buffer.str() << std::endl
-              << "Captured stderr:" << std::endl
-              << m_stderr_buffer.str() << std::endl;
+    const auto test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+    if (test_info && test_info->result()->Failed()) {
+      std::cout << std::endl
+                << "Test failed: " << test_info->name() << std::endl
+                << std::endl
+                << "Captured cout:" << std::endl
+                << m_cout_buffer.str() << std::endl;
+    }
+  }
+}
+
+const std::vector<std::string> &
+BaseTest::getArgs() const {
+  static const auto args { ::testing::internal::GetArgvs() };
+  return args;
+}
+
+std::optional<std::string>
+BaseTest::getArgWithMatchingPattern(const std::string &pattern, bool remove_match) const {
+  const auto &args { getArgs() };
+  if (!args.empty()) {
+    const std::regex re_pattern { pattern };
+
+    // We are skipping the first arg which is always binary name/path.
+    for (auto it { std::next(std::begin(args)) }; it != std::end(args); ++it) {
+      if (std::smatch match; std::regex_search(*it, match, re_pattern)) {
+        return remove_match ? std::regex_replace(*it, re_pattern, "") : *it;
+      }
+    }
   }
 
-  m_sbuf = nullptr;  // clear sbuf
-  if (m_pipe_stdout) {
-    pclose(m_pipe_stdout);
-    m_pipe_stdout = nullptr;
-  }
-  if (m_pipe_stderr) {
-    pclose(m_pipe_stderr);
-    m_pipe_stderr = nullptr;
-  }
+  return std::nullopt;
+}
+
+bool
+BaseTest::isOutputSuppressed() const {
+  return true;
 }
 
 bool
@@ -101,52 +99,30 @@ BaseTest::skipTest() const {
   return {};
 }
 
-int
-BaseTest::exec(const char *cmd) {
-  std::array<char, 128> buffer {};
-  m_pipe_stdout = popen((std::string(cmd) + " 2>&1").c_str(), "r");
-  m_pipe_stderr = popen((std::string(cmd) + " 2>&1").c_str(), "r");
-  if (!m_pipe_stdout || !m_pipe_stderr) {
-    throw std::runtime_error("popen() failed!");
-  }
-  while (fgets(buffer.data(), buffer.size(), m_pipe_stdout) != nullptr) {
-    m_stdout_buffer << buffer.data();
-  }
-  while (fgets(buffer.data(), buffer.size(), m_pipe_stderr) != nullptr) {
-    m_stderr_buffer << buffer.data();
-  }
-  int return_code = pclose(m_pipe_stdout);
-  m_pipe_stdout = nullptr;
-  if (return_code != 0) {
-    std::cout << "Error: " << m_stderr_buffer.str() << std::endl
-              << "Return code: " << return_code << std::endl;
-  }
-  return return_code;
-}
+std::optional<display_device::Logger::LogLevel>
+BaseTest::getDefaultLogLevel() const {
+  const static auto default_log_level {
+    []() -> std::optional<display_device::Logger::LogLevel> {
+      const auto value { getEnv("LOG_LEVEL") };
+      if (value == "verbose") {
+        return display_device::Logger::LogLevel::verbose;
+      }
+      if (value == "debug") {
+        return display_device::Logger::LogLevel::debug;
+      }
+      if (value == "info") {
+        return display_device::Logger::LogLevel::info;
+      }
+      if (value == "warning") {
+        return display_device::Logger::LogLevel::warning;
+      }
+      if (value == "error") {
+        return display_device::Logger::LogLevel::error;
+      }
 
-std::string
-LinuxTest::skipTest() const {
-#ifndef __linux__
-  return "Skipping, this test is for Linux only.";
-#else
-  return BaseTest::skipTest();
-#endif
-}
+      return std::nullopt;
+    }()
+  };
 
-std::string
-MacOSTest::skipTest() const {
-#if !defined(__APPLE__) || !defined(__MACH__)
-  return "Skipping, this test is for macOS only.";
-#else
-  return BaseTest::skipTest();
-#endif
-}
-
-std::string
-WindowsTest::skipTest() const {
-#ifndef _WIN32
-  return "Skipping, this test is for Windows only.";
-#else
-  return BaseTest::skipTest();
-#endif
+  return default_log_level;
 }
