42550692b66daec7d292e6f3b3c2991460aa8300
David Lane <42013603+ReenigneArcher@users.noreply.github.com>
2026-06-10 09:11:03 -0400

    feat: Add helpers for DISPLAYCONFIG desktop image info (#270)

---
+++ src/windows/include/display_device/windows/detail/display_config.h
@@ -0,0 +1,31 @@
+/**
+ * @file src/windows/include/display_device/windows/detail/display_config.h
+ * @brief Declarations for private DisplayConfig compatibility helpers.
+ */
+#pragma once
+
+// local includes
+#include "display_device/windows/types.h"
+
+// system includes
+#include <cstddef>
+#include <cstring>
+
+namespace display_device::detail {
+  [[nodiscard]] inline DISPLAYCONFIG_DESKTOP_IMAGE_INFO getDesktopImageInfo(const DISPLAYCONFIG_MODE_INFO &info) {
+    constexpr std::size_t union_offset {offsetof(DISPLAYCONFIG_MODE_INFO, targetMode)};
+    static_assert(sizeof(DISPLAYCONFIG_MODE_INFO) >= union_offset + sizeof(DISPLAYCONFIG_DESKTOP_IMAGE_INFO));
+
+    DISPLAYCONFIG_DESKTOP_IMAGE_INFO desktop_image_info {};
+    std::memcpy(&desktop_image_info, reinterpret_cast<const std::byte *>(&info) + union_offset, sizeof(desktop_image_info));
+
+    return desktop_image_info;
+  }
+
+  inline void setDesktopImageInfo(DISPLAYCONFIG_MODE_INFO &info, const DISPLAYCONFIG_DESKTOP_IMAGE_INFO &desktop_image_info) {
+    constexpr std::size_t union_offset {offsetof(DISPLAYCONFIG_MODE_INFO, targetMode)};
+    static_assert(sizeof(DISPLAYCONFIG_MODE_INFO) >= union_offset + sizeof(DISPLAYCONFIG_DESKTOP_IMAGE_INFO));
+
+    std::memcpy(reinterpret_cast<std::byte *>(&info) + union_offset, &desktop_image_info, sizeof(desktop_image_info));
+  }
+}  // namespace display_device::detail
